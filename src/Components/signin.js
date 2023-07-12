9896412aa1649641fc575185e35639bb5f0d6598
TemoshoS <temoshomaduane@gmail.vom>
2023-07-12 13:43:54 +0200

    first commit

--- src/Components/signin.js
+++ src/Components/signin.js
@@ -1,7 +1,8 @@
-import React,{ useState } from 'react';
-import { Link ,useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../config/firebase';
+import authimage from '../images/login.jpg'
 
 const Signin = () => {
 
@@ -9,46 +10,44 @@ const Signin = () => {
     const [password, setPassword] = useState('');
     const naviagte = useNavigate();
 
-    const login=()=>{
+    const login = () => {
 
-        signInWithEmailAndPassword(auth, email, password).then(()=>{
+        signInWithEmailAndPassword(auth, email, password).then(() => {
 
             alert('login successfully');
             naviagte('/home');
 
-        }).catch((error)=>{
+        }).catch((error) => {
             console.log(error.message);
 
         })
-        
+
 
     }
 
     return (
-        <div>
+        <div className='auth-card'>
 
-            <div className='login-card'>
+            <div className='auth'>
                 <h2 className='heading'>Sign in</h2>
+                
                 <div className='input-container'>
-
                     <label>email</label>
                     <input type='text' onChange={(event) => setEmail(event.target.value)} />
-
                 </div>
 
                 <div className='input-container'>
                     <label>Password</label>
                     <input type='text' onChange={(event) => setPassword(event.target.value)} />
-
                 </div>
 
                 <button onClick={login}>Login</button>
 
-                <p>Don't have an account ? <Link to='/register' style={{color:'green', textDecoration:'none'}}>Sign up</Link></p>
-
-
+                <p>Don't have an account ? <Link to='/register' style={{ color: 'green', textDecoration: 'none' }}>Sign up</Link></p>
 
             </div>
+
+            <img src={authimage} className='auth-image'/>
 
         </div>
     )
--- src/Components/signup.js
+++ src/Components/signup.js
@@ -1,45 +1,74 @@
 import React, { useState } from 'react'
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../config/firebase';
+import authimage from '../images/login.jpg'
+import { Link } from 'react-router-dom';
 
 
 const Signup = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
 
-  const register=()=>{
+  const register = () => {
 
-    createUserWithEmailAndPassword(auth,email, password).then(()=>{
+    createUserWithEmailAndPassword(auth, email, password).then(() => {
       alert("Registered successfully")
 
-    }).catch((error)=>{
+    }).catch((error) => {
       console.log(error.message)
 
 
     })
 
-  }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
-    <div>
-      <div className="login-card">
+    <div className='auth-card'>
+      <div className='auth'>
+
         <h2 className="heading">Sign up</h2>
+        <p>Register to access your account</p>
 
-        <div className="input-container">
-          <label >Email</label> <br />
-          <input onChange={(e) => setEmail(e.target.value)} />
+
+        <div class="input-container">
+          <label >Name</label>
+          <input type="text" onChange={(e) => setName(e.target.value)} />
         </div>
 
+
         <div className="input-container">
-          <label >Password</label> <br />
-          <input onChange={(e) => setPassword(e.target.value)} />
+          <label >Email</label>
+          <input onChange={(e) => setEmail(e.target.value)} type='text' />
         </div>
 
-          <button onClick={register}>Sign up</button>
+        <div className='input-container'>
+          <label>Password</label>
+          <div className='password-input-container'>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type={showPassword ? 'text' : 'password'}
+            />
+            <i
+              className={`password-toggle-icon ${showPassword ? 'fa fa-eye-slash' : 'fa fa-eye'}`}
+              onClick={togglePasswordVisibility}
+            ></i>
+          </div>
+        </div>
 
+
+        <button className='submit-button' onClick={register}>SIGNUP</button>
+        <p style={{ marginLeft: '130px' }}>Have an account?<Link to='/' className='login-link' >LOGIN</Link></p>
 
       </div>
+      <img src={authimage} className='auth-image' />
+
 
     </div>
   )

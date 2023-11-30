754b60d48e9a6403a76e44baf660dcbce249da5b
TemoshoS <temoshomaduane@gmail.vom>
2023-11-30 15:02:51 +0200

    footer

--- src/Components/book.js
+++ src/Components/book.js
@@ -9,15 +9,15 @@ import Footer from './footer';
 
 const Book = () => {
     const navigate = useNavigate();
-    const { itemId } = useParams(); 
+    const { itemId } = useParams();
     const [room, setRoom] = useState(null);
-    
-    
+
+
     useEffect(() => {
-        
+
         const fetchRoom = async () => {
             try {
-             
+
                 const roomData = await fetchRoomDetails(itemId);
                 setRoom(roomData);
             } catch (error) {
@@ -29,41 +29,56 @@ const Book = () => {
     }, [itemId]);
 
 
-   const bookRoom = async () => {
-    try {
-        const cartData = await bookHotel(itemId);
-        setRoom(cartData);
-        navigate('/successful');
-    } catch (error) {
-        console.log(error.message);
-    }
-};
+    const bookRoom = async () => {
+        try {
+            const cartData = await bookHotel(itemId);
+            setRoom(cartData);
+            navigate('/successful');
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
 
 
     return (
         <div>
-            <NavBar/>
-        <div className='reserve-card'>
-          <img src={room && room.roomImage ? room.roomImage : ''} className='room-view' alt='roomview' />
-          <img src={room && room.roomImage ? room.roomImage : ''} className='room-view1' alt='roomview' />
-          <img src={room && room.roomImage ? room.roomImage : ''} className='room-view2' alt='roomview' />
-          <img src={room && room.roomImage ? room.roomImage : ''} className='room-view3' alt='roomview' />
-          <div>
-            <h2 className='room-name'>{room ? room.roomName : ''}</h2>
-          </div>
-          <p className='reserve-description'>{room ? room.roomDescription : ''}</p>
-          <h3 className='roomPrice'>
-            <span>R {room ? room.roomPrice : ''}</span>
-            <span className='total'>total</span>
-          </h3>
-          <button className='reserve-button' onClick={bookRoom}>
-            RESERVE
-          </button>
-        </div>
+            <NavBar />
+            <div className='reserve-card'>
+                <div className='images-container'>
+                    <div>
+                        <img src={room && room.roomImage ? room.roomImage : ''} className='room-view' alt='roomview' />
+                    </div>
+                    <div className='room-container'>
+                        <img src={room && room.roomImage ? room.roomImage : ''} className='room-view1' alt='roomview' />
+                        <img src={room && room.roomImage ? room.roomImage : ''} className='room-view2' alt='roomview' />
+                        <img src={room && room.roomImage ? room.roomImage : ''} className='room-view3' alt='roomview' />
 
-        <Footer/>
-      </div>
+                    </div>
+
+                    <div>
+                        <h3 className='roomPrice'>
+                            <span>R {room ? room.roomPrice : ''}</span>
+
+                        </h3><
+                            button className='reserve-button' onClick={bookRoom}>
+                            RESERVE
+                        </button>
+                    </div>
+                </div>
+
+                <div>
+                </div>
+
+                <h2 className='room-name'>{room ? room.roomName : ''}</h2>
+
+                <p className='reserve-description'>{room ? room.roomDescription : ''}</p>
+
+
+            </div>
+
+            <Footer />
+        </div>
     )
 }
 
